f22150325fc0d4201d5ce882605203771c194aa4
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-10-17 10:31:54 -0700

    insert user-roughts.js

---
+++ routes/user-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriedn,
+  removeFriend,
+} = require("../../controllers/user-controller");
+
+// Set up GET all and POST
+router.route("/").get(getAllUser).post(createUser);
+
+// Set up GET one, PUT, and DELETE
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// Set up POST friend, and DELETE friend
+router.route("/:id/friends/:friendID").post(addFriend).delete(removeFriend);
+
+module.exports = router;
