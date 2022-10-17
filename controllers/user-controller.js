eb6ae5545144e1ae20579be65fc5f0d9c3542be1
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-10-17 15:39:09 -0700

    update user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,43 @@
 const { User, Thought } = require("../models");
 
-const userController = {  ///<-------------------
+const userController = {  
   getAllUser(req, res) {
-    User.find()
+    User.find({})
+        .populate({
+          path: 'thought',
+          select: '-__v'
+        })
+        .populate({
+          path: 'friend',
+          select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
   },
-  getUserById({ params }, res) {}, //<---------------------
+
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+        .populate({
+          path: 'thought',
+          select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+          if(!dbUserData) {
+            res.status(404).json({ message: 'No user found at this id' });
+            return;
+          }
+          res.json(dbUserData)
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err)
+        })
+  },
 
   createUser({ body }, res) {
     User.create(body)
@@ -33,7 +66,7 @@ const userController = {  ///<-------------------
   },
 
   deleteUser({ params }, res) {},  //<----------------------
-  
+
   addFriend({ params }, res) {
     User.findOneAndUpdate(
       { id: params.userId },  //<<<<-----------Where should I init userId?
