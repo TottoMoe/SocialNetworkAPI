a615147db745341ced7e392d0d36f6e78c4c8f4e
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-10-17 12:11:02 -0700

    update thought-controlelr.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,7 +4,27 @@ const thoughtsControllers = {
   getAllThought(req, res) {},
   getThoughtById({ params }, res) {},
   updateThoughtById({ body, params }, res) {},
-  addThought({ body }, res) {},
+  addThought({ body }, res) {
+    Thought.create(body)
+      .then(({ username, _id }) => {
+        return User.findByIdAndUpdate(
+          { username: username },
+          { $push: { thought: _id } },
+          { new: true, runValidators: true }
+        )
+      })
+      .then(dbUserData => {
+        if(!dbUserData) {
+          res.status(404).json({ message: 'No user found at this id! '});
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
   removeThought({ params }, res) {},
   addReaction({ params, body }, res) {},
   removeReaction({ params }, res) {},
